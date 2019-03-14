ff5531b22e4bb67f27206af5d068d5377b0ab1cf
Katherine Fleming <katherinemarief@gmail.com>
2019-03-14 18:56:36 -0500

    commit

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 $(document).ready(function () {
 
-    
+    //made the following happen when button is clicked
+
     $("#btnSubmit").click(function () {
-        let $inputText = $("#msg").val
-        if ($("#msg").val !== "") { //check for text input in the textbox
-            $("#btnSubmit").prop("disabled", false);//setting disabled attribute on html element to disabled
-        } else { //if the user typesin the input field...
-            $("#btnSubmit").prop("disabled", true);//setting disabled=disabled attribute to false in HTML
-            alert($inputText); //alert the value of the input
-            $("<div></div>").appendTo("body"); //created a div element and appended to the body of document
-            $("div").append("<h2></h2>");
-            $("h2").append($inputText);
+        $("btnSubmit").prop("disabled", true);
+            if ($("inputText") === "") {
+                $("btnSubmit").prop("disabled", true)
+            } else {
+                 alert($("#inputText").val());
+                 ("h2").append($("#inputText").val());
+
         }
+        $("<div></div>").appendTo("body");
+        $("<h2></h2>").appendTo("div");
 
-  //when user mouses over h2, assign it a new bgcolor and border radius
-      
-      
-    })
 
-    
+    });
+
 
 });
+
