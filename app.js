0c7c6790083b2451ee9351873e70aa3621afe0fc
Katherine Fleming <katherinemarief@gmail.com>
2019-03-13 22:35:55 -0500

    deletions

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 $(document).ready(function () {
 
-
-    $("btnSubmit").click(function () {
+    
+    $("#btnSubmit").click(function () {
         let $inputText = $("#msg").val
-        $inputText.keyup(); {
-            if ($inputText !== "") {
-                $("#btnSubmit").prop("disabled", false);//setting disabled attribute on html element to disabled
-            } else {
-                $("#btnSubmit").prop("disabled", true);//setting disabled=disabled attribute to false in HTML
-                alert ($inputText); //alert the value of the input
-                $("<div></div>").appendTo("body"); //created a div element and appended to the body of document
-                $("div").append("<h2></h2>");
-                $("h2").append($inputText);
-            }
+        if ($("#msg").val !== "") { //check for text input in the textbox
+            $("#btnSubmit").prop("disabled", false);//setting disabled attribute on html element to disabled
+        } else { //if the user typesin the input field...
+            $("#btnSubmit").prop("disabled", true);//setting disabled=disabled attribute to false in HTML
+            alert($inputText); //alert the value of the input
+            $("<div></div>").appendTo("body"); //created a div element and appended to the body of document
+            $("div").append("<h2></h2>");
+            $("h2").append($inputText);
         }
+
+  //when user mouses over h2, assign it a new bgcolor and border radius
+      
+      
     })
 
-
-
-
-
-        //when user mouses over h2, assign it a new bgcolor and border radius
-
-
+    
 
 });
