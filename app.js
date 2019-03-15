43e0244c3778be0d05afa3b28f176c7634b8c9b2
Katherine Fleming <katherinemarief@gmail.com>
2019-03-14 19:49:51 -0500

    idk

--- app.js
+++ app.js
@@ -8,13 +8,22 @@ $(document).ready(function () {
                 $("btnSubmit").prop("disabled", true)
             } else {
                  alert($("#inputText").val());
+                 $("<div></div>").appendTo("body");
+                 $("<h2></h2>").appendTo("div");
                  ("h2").append($("#inputText").val());
 
         }
-        $("<div></div>").appendTo("body");
-        $("<h2></h2>").appendTo("div");
+     $("<ul></ul>").appendTo("body");
+     let text = $("#inputText").val()
+     $("ul").append("<li></li")
 
+$("h2").mouseover(function(){
+$("h2").css({
+    "background-color": "purple",
+    "border-radius": "5px"
 
+})
+})
     });
 
 
